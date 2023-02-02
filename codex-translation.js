af1de5e889dc0dd148d01f0d5b94bad46ff59ae7
Zihong Chen <zihch@microsoft.com>
2023-02-02 10:14:45 +0800

    feat: init

---
+++ codex-translation.js
@@ -0,0 +1,33 @@
+async function main() {
+    c
+    const { Configuration, OpenAIApi } = require("openai");
+
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    
+    const response = await openai.createCompletion({
+        model: "code-davinci-002",
+        prompt: `
+            #JavaScript to Python:
+            JavaScript:
+dogs = ["bill", "joe", "carl"]
+car = []
+dogs.forEach((dog) {
+    car.push(dog);
+});
+
+        Python:`,
+        temperature: 0,
+        max_tokens: 128,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+        stop: ["#"],
+    });
+
+    console.log(response.data.choices[0].text);
+}
+
+main(); 
---
+++ image.js
@@ -0,0 +1,19 @@
+async function main() {
+    require('dotenv').config()
+    const { Configuration, OpenAIApi } = require("openai");
+
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    
+    const response = await openai.createImage({
+        prompt: "用需要中国山水画给这首诗配图。寂静夜色深，离愁难再寻。心中留给谁，此生离别音。",
+        n: 10,
+        size: "1024x1024",
+    });
+
+    console.log(response.data.data);
+}
+
+main();
