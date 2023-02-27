cf501177fa0c78d4a0f8639b13575dfcdfb9054f
Zihong Chen <zihch@microsoft.com>
2023-02-27 14:54:03 +0800

    chore: add word count function

--- src/summarize.ts
+++ src/summarize.ts
@@ -2,8 +2,8 @@ import * as pdfjsLib from "pdfjs-dist";
 import { Configuration, OpenAIApi } from "openai";
 
 const pdfUrl = "https://www.arvindguptatoys.com/arvindgupta/oldmansea.pdf";
-const pdfChunkSize = 1500;
-const maxSummaryLength = 200;
+const pdfChunkSize = 1200;
+const maxSummaryLength = 500;
 const maxTokens = 2048;
 
 const configuration = new Configuration({
@@ -17,10 +17,13 @@ async function getPdfAsText(url) {
   const pdf = await loadingTask.promise;
   const pdfChunk = [];
 
-  for (let i = 1; i <= 10; i++) {
+  for (let i = 1; i <= pdf.numPages; i++) {
     const page = await pdf.getPage(i);
     const content = await page.getTextContent();
     const text = content.items.map((item) => item.str).join(" ");
+    console.log(
+      `--------------------- Page ${i} -------------------------\n ${text} `
+    );
     pdfChunk.push(text);
   }
 
@@ -47,11 +50,14 @@ async function combine(originalText: string[], targetText = []) {
   let toSummarizeText = "";
   while (originalText.length > 1) {
     toSummarizeText += originalText.shift();
-    if (toSummarizeText.length > pdfChunkSize) {
+    if (countWords(toSummarizeText) > pdfChunkSize) {
       console.log(
-        `--------------------- To Summarize Text -------------------------\n ${toSummarizeText} `
+        `--------------------- To Summarize Text ${countWords(
+          toSummarizeText
+        )} -------------------------\n ${toSummarizeText} `
       );
-      const response = await summarize(toSummarizeText);
+      //const response = await summarize(toSummarizeText);
+      const response = "This is a test";
       console.log(
         `--------------------- Response From AI -------------------------\n ${response}`
       );
@@ -65,6 +71,11 @@ async function combine(originalText: string[], targetText = []) {
   //targetText.push(response);
 
   return combine(targetText, []);
+}
+
+function countWords(str: string): number {
+  const words = str.split(/\s+/);
+  return words.length;
 }
 
 async function main() {
